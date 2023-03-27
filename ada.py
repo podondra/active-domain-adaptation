4e551e41f610ce7b10d5d799b7ef6247e431b8bd
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2023-03-27 13:49:44 +0200

    Work on inverse problem and PIT histogram

--- ada.py
+++ ada.py
@@ -4,7 +4,6 @@ from random import randint
 import click
 import numpy as np
 import pandas as pd
-from scipy.stats import norm
 from sklearn.model_selection import train_test_split
 import torch
 from torch import nn
@@ -15,7 +14,9 @@ from torch.utils.data import DataLoader, TensorDataset
 import wandb
 
 
+BINS = 32
 BS = 2048   # batch size for testing
+CONFIDENCES = torch.linspace(0.0, 1.0, steps=11)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 EPS = 1e-6
 MODELPATH = "models/{}.pt"
@@ -24,20 +25,12 @@ SPLIT_COLUMN = "wtd_std_Valence"
 TARGET_COLUMN = "critical_temp"
 
 
-def f(x):
-    return x + 0.3 * torch.sin(2 * math.pi * x)
-
-
-def get_forward(n=1000):
-    torch.manual_seed(13)
-    x = torch.empty(n, 1).uniform_(0.0, 1.0)
-    y = f(x) + torch.empty(n, 1).normal_(std=0.1)
+def get_inverse(n=1000, seed=13):
+    rng = np.random.default_rng(seed)
+    y = rng.uniform(0.0, 1.0, size=(n, 1))
+    x = y + 0.3 * np.sin(2 * math.pi * y) + rng.uniform(-0.1, 0.1, size=(n, 1))
+    y += rng.normal(scale=0.05, size=(n, 1))
     return None, x, y
-
-
-def get_inverse(n=1000):
-    _, x, y = get_forward(n)
-    return None, y, x
 
 
 def get_protein(datapath="data/CASP.csv"):
@@ -45,6 +38,7 @@ def get_protein(datapath="data/CASP.csv"):
     return df, df.values[:, 1:], df.values[:, :1]
 
 
+# TODO drop SPLIT_COLUMN elsewhere
 def get_superconductivity(datapath="data/superconduct/train.csv"):
     """The superconductivity data set is available on-line,
     at https://archive.ics.uci.edu/ml/datasets/Superconductivty+Data.
@@ -72,7 +66,6 @@ def get_year(datapath="data/YearPredictionMSD.txt"):
 
 
 DATASETS = {
-        "forward": get_forward,
         "inverse": get_inverse,
         "protein": get_protein,
         "wine": get_wine,
@@ -119,17 +112,21 @@ def data_preparation(arrays):
 def numpy2torch(array):
     return torch.from_numpy(array.astype(np.float32))
 
+
 def to_device(tensor):
     return tensor.to(DEVICE)
 
-def get_dataset(dataname, seed, validation):
+
+def get_dataset(dataname, seed, validation, preparation):
     df, X, y = DATASETS[dataname]()
     X, y = numpy2torch(X), numpy2torch(y)
     Xs, ys = test_split(X, y, 0.1, seed)
     if validation:    # split the training set and return validation set (not test set)
         Xs, ys = test_split(Xs[0], ys[0], 0.2, seed)
-    Xs, X_scaler = data_preparation(Xs)
-    ys, y_scaler = data_preparation(ys)
+    X_scaler, y_scaler = None, None
+    if preparation:
+        Xs, X_scaler = data_preparation(Xs)
+        ys, y_scaler = data_preparation(ys)
     datasets = map(TensorDataset, map(to_device, Xs), map(to_device, ys))
     return tuple(datasets), (X_scaler, y_scaler)
 
@@ -145,7 +142,7 @@ def get_datasets():
     Xs, _ = data_preparation(Xs_source + Xs_target)    # "+" chains tuples
     ys, y_scaler = data_preparation(ys_source + ys_target)
     datasets = map(TensorDataset, map(to_device, Xs), map(to_device, ys))
-    return tuple(datasets), y_scaler
+    return tuple(datasets), (X_scaler, y_scaler)
 
 
 def get_y(dataset):
@@ -220,6 +217,21 @@ def normal_pdf(x, mean, variance):
     return (1.0 / (torch.sqrt(2.0 * math.pi * variance))) * torch.exp(-0.5 * ((x - mean) ** 2 / variance))
 
 
+def normal_quantile(x, mean, variance):
+    return mean + torch.sqrt(variance) * math.sqrt(2) * torch.erfinv(2 * x - 1)
+
+
+def normal_reliability(mean, variance, y):
+    lower = [normal_quantile((1 - p) / 2, mean, variance) for p in CONFIDENCES]
+    upper = [normal_quantile(1 - ((1 - p) / 2), mean, variance) for p in CONFIDENCES]
+    return [torch.sum((l < y) & (y < u)) / len(y) for l, u in zip(lower, upper)]
+
+
+def calibration_curve(ax, reliabilities):
+    ax.plot(CONFIDENCES, reliabilities, marker="o")
+    ax.plot(CONFIDENCES, CONFIDENCES)
+
+
 def crps(mean, variance, y):
     variance = clamp(variance)
     std = torch.sqrt(variance)
@@ -231,30 +243,26 @@ def se(mean, variance, y):
     return (y - mean) ** 2
 
 
-def pit(mean, variance, y):
+def normal_pit(mean, variance, y):
     variance = variance.clamp(min=EPS)
     return normal_cdf(y, mean, variance)
 
 
-class Network(Model):
+class NeuralNetwork(Model):
     def __init__(self, features, hyperparams):
         super().__init__()
         self.loss_function = {"crps": crps, "nll": nll, "se": se}[hyperparams["loss"]]
         self.model = nn.Sequential(
             nn.Linear(features, hyperparams["neurons"]),
             nn.ReLU(),
-            nn.Linear(hyperparams["neurons"], 1))
-        self.model_variance = nn.Sequential(
-            nn.Linear(features, hyperparams["neurons"]),
-            nn.ReLU(),
-            nn.Linear(hyperparams["neurons"], 1))
+            nn.Linear(hyperparams["neurons"], 2))
         self.to(DEVICE)
 
     def forward(self, X):
         # torch.exp versus F.softplus output function for variance
         # mdn: exp give nans
-        mean = self.model(X)
-        variance = F.softplus(self.model_variance(X))
+        output = self.model(X)
+        mean, variance = output[:, :1], F.softplus(output[:, 1:])
         return mean, variance
 
     @torch.no_grad()
@@ -266,14 +274,15 @@ class Network(Model):
 
     def metrics(self, y_pred, y, y_scaler):
         mean, variance = y_pred
-        mean = y_scaler.inverse_transform(mean)
-        variance = y_scaler.inverse_transform_variance(variance)
-        y = y_scaler.inverse_transform(y)
+        if y_scaler is not None:
+            mean = y_scaler.inverse_transform(mean)
+            variance = y_scaler.inverse_transform_variance(variance)
+            y = y_scaler.inverse_transform(y)
         return {
                 "crps": torch.mean(crps(mean, variance, y)).item(),
                 "mae": torch.mean(torch.abs(mean - y)).item(),
                 "nll": torch.mean(nll(mean, variance, y)).item(),
-                "pit": wandb.Histogram(pit(mean, variance, y), num_bins=32),
+                "pit": wandb.Histogram(normal_pit(mean, variance, y), num_bins=BINS),
                 "rmse": torch.sqrt(torch.mean(torch.square(mean - y))).item(),
                 "sharpness": torch.mean(variance).item(),
                 "variance": wandb.Histogram(variance)}
@@ -299,6 +308,13 @@ def gmm_nll(coeffs, means, variances, y):
     return -torch.log(torch.sum(coeffs * normal_pdf(y, means, variances), dim=-1, keepdim=True))
     #coeffs = clamp(coeffs)
     #return -torch.logsumexp(torch.log(coeffs) - 0.5 * torch.log(2.0 * math.pi * variances) - 0.5 * ((y - means) ** 2 / variances), dim=-1, keepdim=True)
+
+def gmm_cdf(x, coeffs, means, variances):
+    return torch.sum(coeffs * normal_cdf(x, means, variances), dim=-1, keepdim=True)
+
+
+def gmm_pdf(x, coeffs, means, variances):
+    return torch.sum(coeffs * normal_pdf(x, means, variances), dim=-1, keepdim=True)
 
 
 def gmm_pit(coeffs, means, variances, y):
@@ -333,18 +349,19 @@ class MixtureDensityNetwork(Model):
 
     def metrics(self, y_pred, y, y_scaler):
         coeffs, means, variances = y_pred
-        means = y_scaler.inverse_transform(means)
-        variances = y_scaler.inverse_transform_variance(variances)
-        y = y_scaler.inverse_transform(y)
+        if y_scaler is not None:
+            means = y_scaler.inverse_transform(means)
+            variances = y_scaler.inverse_transform_variance(variances)
+            y = y_scaler.inverse_transform(y)
         return {
                 "crps": torch.mean(gmm_crps(coeffs, means, variances, y)).item(),
                 "nll": torch.mean(gmm_nll(coeffs, means, variances, y)).item(),
-                "pit": wandb.Histogram(gmm_pit(coeffs, means, variances, y), num_bins=32)}
+                "pit": wandb.Histogram(gmm_pit(coeffs, means, variances, y), num_bins=BINS)}
 
 
 class DeepEnsemble:
     def __init__(self, features, hyperparams):
-        self.models = [Network(features, hyperparams) for _ in range(hyperparams["m"])]
+        self.models = [NeuralNetwork(features, hyperparams) for _ in range(hyperparams["m"])]
 
     @torch.no_grad()
     def predict(self, dataset):
@@ -391,9 +408,11 @@ class DeepEnsemble:
 @click.option("--neurons", default=50, help="Number of neurons in the first hidden layer.")
 @click.option("--m", default=1, help="Number of model in deep ensemble.")
 @click.option("--modelname", type=click.Path(exists=True, dir_okay=False))
+@click.option("--preparation", is_flag=True)
 @click.option("--seed", default=50, help="Seed for train and test set split.")
 @click.option("--step", default=1, help="Period of learning rate decay.")
 @click.option("--validation", is_flag=True)
+@click.option("--watch", is_flag=True)
 @click.option("--wd", default=0.0, help="Weight decay.")
 def train(**hyperparams):
     # reproducibility
@@ -403,22 +422,24 @@ def train(**hyperparams):
     # datasets, y_scaler = get_datasets()
     # trainset_source, testset_source, _, _ = datasets
     (trainset, testset), (_, y_scaler) = get_dataset(
-            hyperparams["dataname"], hyperparams["seed"], hyperparams["validation"])
-    features = trainset.tensors[0].shape[1]
+            hyperparams["dataname"], hyperparams["seed"],
+            hyperparams["validation"], hyperparams["preparation"])
     # generate random run name with loss information
     runname = "{}-{}-{}-{}".format(
             hyperparams["loss"], hyperparams["m"], hyperparams["k"], randint(1000, 9999))
-    with wandb.init(config=hyperparams, name=runname, project="uci"):
+    with wandb.init(config=hyperparams, name=runname):
         config = wandb.config
+        features = trainset.tensors[0].shape[1]
         if config["m"] > 1:
             model = DeepEnsemble(features, config)
         elif config["k"] > 1:
             model = MixtureDensityNetwork(features, config)
         else:
-            model = Network(features, config)
+            model = NeuralNetwork(features, config)
         if config["modelname"] is not None:
             model.load(config["modelname"])
-        wandb.watch(model)
+        if config["watch"]:
+            wandb.watch(model)
         model.train_epochs(trainset, testset, config, y_scaler)
         model.save(runname)
 
