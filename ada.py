3e506626dbe03c1d6f6d00c142b33fc47e9ac618
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2023-05-10 09:10:01 +0200

    Back up changes

--- ada.py
+++ ada.py
@@ -172,6 +172,17 @@ def get_power(datapath="data/Folds5x2_pp.xlsx"):
 
 
 def get_protein(datapath="data/CASP.csv"):
+    """https://archive.ics.uci.edu/ml/datasets/Physicochemical+Properties+of+Protein+Tertiary+Structure
+    RMSD-Size of the residue.
+    F1 - Total surface area.
+    F2 - Non polar exposed area.
+    F3 - Fractional area of exposed non polar residue.
+    F4 - Fractional area of exposed non polar part of residue.
+    F5 - Molecular mass weighted exposed area.
+    F6 - Average deviation from standard exposed area of residue.
+    F7 - Euclidian distance.
+    F8 - Secondary structure penalty.
+    F9 - Spacial Distribution constraints (N,K Value)."""
     df = pd.read_csv(datapath, dtype=np.float32)
     return df, df.values[:, 1:], df.values[:, :1]
 
